90c68d2126513d40620307fd8f550e9b2ea1e9cf
kayjeee <kagiso.killagram@gmail.com>
2025-10-08 13:01:01 +0200

    update render

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,18 @@
 
 set -o errexit
 
+echo "Unfreezing bundle..."
+bundle config set frozen false
+
 echo "Installing dependencies..."
 bundle install
+
+echo "Setting up secret key base..."
+# Generate secret key if not set
+if [ -z "$SECRET_KEY_BASE" ]; then
+  export SECRET_KEY_BASE=$(rails secret)
+  echo "Generated SECRET_KEY_BASE"
+fi
 
 echo "Precompiling assets..."
 bundle exec rails assets:precompile
