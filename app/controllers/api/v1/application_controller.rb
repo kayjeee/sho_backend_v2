0ecf732b26f44e5b1ccabb4b1529cc77b0a58984
kayjee <Kagiso.killagram@gmail.com>
2025-10-17 21:07:19 +0200

    ok

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,7 +2,8 @@
 module Api
   module V1
     class ApplicationController < ::ApplicationController
-      protect_from_forgery with: :null_session
+      # For API-only apps, forgery protection isn’t used
+      # You can handle authentication or CSRF tokens manually if needed
     end
   end
 end
