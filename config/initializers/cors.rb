ba97de9d3b695e10cb38d51cac78a696d3fd4c04
kayjee <Kagiso.killagram@gmail.com>
2026-01-14 17:02:43 +0200

    update
    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    # ✅ Include both www and non-www domains
-    origins 'https://www.schoolheadoffice.com',
-            'https://schoolheadoffice.com',
-            'https://schoolheadoffficeinvitations.vercel.app',
-            'http://localhost:3000'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+    allow do
+      origins '*' # Change this to your frontend URL in production
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
-end
+  
